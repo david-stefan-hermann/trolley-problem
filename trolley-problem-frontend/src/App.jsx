77cab15ebaf0c2e9508ba36297cb07b1f3e373e3
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-16 18:49:20 +0200

    added dashboard

--- trolley-problem-frontend/src/App.jsx
+++ trolley-problem-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 // import './App.css'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import Home from './components/home/Home'
 import Dashboard from './components/dashboard/Dashboard'
 
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   )
--- trolley-problem-frontend/src/components/dashboard/dashboard.jsx
+++ trolley-problem-frontend/src/components/dashboard/dashboard.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import scenarios from '../scenarios/scenarios_data'
 
 const Dashboard = () => {
+  const [responses, setResponses] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/responses')
+        const data = await response.json()
+        setResponses(data)
+      } catch (error) {
+        console.error('Error fetching responses:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the Dashboard!</p>
+    <div className="p-4">
+      <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+
+      {scenarios?.map(scenario => {
+
+        const response = responses?.find(res => res.scenarioId === scenario.id)
+        return (
+
+          <div key={scenario?.id} className="mb-6">
+            <h2 className="text-xl mb-2">{scenario?.question}</h2>
+
+            <div className="ml-4">
+              {scenario?.outcomes?.map((outcome, index) => {
+
+                const voteCount = response ? response.votes[`outcome${index + 1}`] : 0
+                return (
+                  <div key={index} className="mb-1">
+                    <span className="font-semibold">{outcome.option}: </span>
+                    <span>{voteCount} Stimmen</span>
+                  </div>
+                )
+              })}
+            </div>
+
+          </div>
+        )
+
+
+      })}
     </div>
   )
 }
