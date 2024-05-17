63b9e8ff27de3c51b01dacc1fea4e486b153598c
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-18 00:53:25 +0200

    fixing dev and prod env

--- trolley-problem-backend/server.js
+++ trolley-problem-backend/server.js
@@ -3,10 +3,20 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import responseRoutes from './routes/responses.js'
 import dotenv from 'dotenv'
-dotenv.config()
+
+
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
+dotenv.config({ path: envFile })
 
 const app = express()
 const port = process.env.PORT || 5000
+
+const mongoUri = process.env.MONGO_URI
+
+if (!mongoUri) {
+  console.error('MONGO_URI is not defined')
+  process.exit(1)
+}
 
 // Middleware
 app.use(cors())
@@ -15,7 +25,7 @@ app.use(express.json())
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(mongoUri)
     const connectionState = mongoose.connection.readyState
     if (connectionState === 1) {
       console.log('MongoDB connected successfully')
--- trolley-problem-frontend/vite.config.js
+++ trolley-problem-frontend/vite.config.js
@@ -1,29 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist'
-//   }
-// })
+import dotenv from 'dotenv'
+
+// Load environment variables based on the current mode
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
 
 // https://thedkpatel.medium.com/dockerizing-react-application-built-with-vite-a-simple-guide-4c41eb09defa
 export default defineConfig({
   base: "/",
   plugins: [react()],
   preview: {
-    port: 80,
+    port: process.env.VITE_SERVER_PORT || 80,
     strictPort: true,
   },
   server: {
     watch: {
       usePolling: true,
     },
-    port: 80,
+    port: process.env.VITE_SERVER_PORT || 80,
     strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:80",
+    host: process.env.VITE_SERVER_HOST || true,
+    origin: `http://${process.env.VITE_SERVER_HOST}:${process.env.VITE_SERVER_PORT}`,
+  },
+  build: {
+    outDir: 'dist',
   },
 })
