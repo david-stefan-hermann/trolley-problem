5618b39ff3dd3225be251445264016314c77c830
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-17 19:06:33 +0200

    trying online article

--- trolley-problem-frontend/vite.config.js
+++ trolley-problem-frontend/vite.config.js
@@ -2,9 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist'
+//   }
+// })
+
+// https://thedkpatel.medium.com/dockerizing-react-application-built-with-vite-a-simple-guide-4c41eb09defa
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-})
+  preview: {
+   port: 80,
+   strictPort: true,
+  },
+  server: {
+   port: 80,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:80",
+  },
+ })
