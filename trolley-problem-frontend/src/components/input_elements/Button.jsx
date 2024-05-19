e6dbe69061fe1494b4563fb6c8e712ebe9691791
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-19 02:34:43 +0200

    button responisve mobile

--- trolley-problem-frontend/src/components/input_elements/Button.jsx
+++ trolley-problem-frontend/src/components/input_elements/Button.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Button = ({ onClick, children }) => (
     <button
-        className="font-bold bg-white text-black border-2 border-black py-2 px-2 rounded-lg transform md:hover:bg-black md:hover:text-white transition-colors duration-300 ease-in-out"
+        className="font-bold bg-white text-black border-2 border-black py-2 px-2 rounded-lg transform hover:bg-black hover:text-white transition-colors duration-300 ease-in-out active:scale-95 active:bg-gray-300"
         onClick={onClick}
     >
         {children}
     </button>
+
 )
 
 export default Button
