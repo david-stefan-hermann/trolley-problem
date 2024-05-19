242ddd7ef3882f8eb4f949b9acd830c617ced856
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-19 23:49:59 +0200

    hopefully fixed hover

--- trolley-problem-frontend/src/components/input_elements/Button.jsx
+++ trolley-problem-frontend/src/components/input_elements/Button.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 const Button = ({ onClick, children }) => (
     <button
-        className="font-bold bg-white text-black border-2 border-black py-2 px-2 rounded-lg transform hover:bg-black hover:text-white transition-colors duration-300 ease-in-out active:scale-95 active:bg-gray-300"
+        className="font-bold bg-white text-black border-2 border-black py-2 px-2 rounded-lg transform betterhover:hover:bg-black betterhover:hover:text-white transition-colors duration-300 ease-in-out active:scale-95 active:bg-gray-300"
         onClick={onClick}
     >
         {children}
     </button>
-
 )
 
 export default Button
--- trolley-problem-frontend/tailwind.config.js
+++ trolley-problem-frontend/tailwind.config.js
@@ -4,12 +4,17 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'indie': ['Indie Flower', 'sans-serif'], // Name it as 'indie' or any name you prefer
-        'annie': ['Annie Use Your Telescope', 'sans-serif'], // Name it as 'annie' or any name you prefer
-        'playpen': ['Playpen Sans', 'sans-serif'] // Name it as 'playopen' or any name you prefer
+        'indie': ['Indie Flower', 'sans-serif'],                // Name it as 'indie' or any name you prefer
+        'annie': ['Annie Use Your Telescope', 'sans-serif'],    // Name it as 'annie' or any name you prefer
+        'playpen': ['Playpen Sans', 'sans-serif']               // Name it as 'playopen' or any name you prefer
+      },
+      screens: {
+        'betterhover': { 'raw': '(hover: hover)' },
       }
     },
   },
   plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true
+  }
 }
-
