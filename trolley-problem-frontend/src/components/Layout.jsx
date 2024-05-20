19a4010e12de612f9470c3670000b7a5788c3f11
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-20 03:48:03 +0200

    added fa, changed footer

--- trolley-problem-frontend/src/components/Layout.jsx
+++ trolley-problem-frontend/src/components/Layout.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa"
+
 
 const Layout = ({ children }) => {
     return (
@@ -20,7 +22,12 @@ const Layout = ({ children }) => {
                     {' '} &#x2013; {' '}
                     <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href='https://tailwindcss.com/docs'>Tailwind CSS</a>
                 </span>
-                <p className="text-1xl">&copy;2024 Karo, Philipp und David</p>
+                
+                <p>
+                    <FaGithub className='inline mr-1 align-baseline' />
+                    <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href='https://github.com/david-stefan-hermann/trolley-problem'>David Hermann</a>
+                    , Karo, Philipp
+                </p>
             </footer>
         </div>
     )
