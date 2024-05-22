50bfc4ab5d3588df3da7034862307461d49f7a22
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-23 01:12:43 +0200

    added spinning sound logic

--- trolley-problem-frontend/src/components/fortunewheel/FortuneWheel.jsx
+++ trolley-problem-frontend/src/components/fortunewheel/FortuneWheel.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import CircleSegments from './CircleSegments'
 import Buttons from '../input_elements/Buttons'
 import Button from '../input_elements/Button'
 import segments from './fortune_wheel_data'
 import IconArrowUp from '../../assets/icons/icon-arrow-up.png'
 import confetti from 'canvas-confetti'
+import confettiSound from '../../assets/sounds/firecracker.mp3'
+import spinningSound from '../../assets/sounds/bike-wheel.mp3'
 
 
 function FortuneWheel() {
@@ -12,6 +14,8 @@ function FortuneWheel() {
     const [degree, setDegree] = useState(0)
     const [winningSegment, setWinningSegment] = useState('') // The segment that the wheel stops at
     const canvasRef = useRef(null) // Ref for the confetti canvas
+    const audioRef = useRef(null)
+    const spinningRef = useRef(null)
 
     const spinWheel = () => {
         setWheelSpinning(true)
@@ -20,6 +24,7 @@ function FortuneWheel() {
         const newDegree = degree + additionalDegrees // Add to current degree for continuous rotation
 
         setDegree(newDegree)
+        playSound(spinningRef)
 
         setTimeout(() => {
             setWheelSpinning(false)
@@ -27,7 +32,8 @@ function FortuneWheel() {
             setDegree(normalizedDegree)
             calculateWinner(normalizedDegree)
             launchConfetti() // Trigger confetti on stop
-
+            playSound(audioRef)
+            pauseSound(spinningRef)
         }, 3000)
     }
 
@@ -39,15 +45,38 @@ function FortuneWheel() {
 
     const launchConfetti = () => {
         confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 } // Makes it look like it's coming from the bottom
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 } // Makes it look like it's coming from the bottom
         })
     }
+
+    const playSound = (ref) => {
+        if (ref.current) {
+            ref.current.currentTime = 0
+            ref.current.play()
+        }
+    }
+
+    const pauseSound = (ref) => {
+        if (ref.current) {
+            ref.current.pause()
+        }
+    }
+
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.volume = 0.1
+        }
+        if (spinningRef.current) {
+            spinningRef.current.volume = 0.3
+        }
+    }, [])
 
     return (
         <div className="w-full flex flex-col text-center items-center">
             <canvas ref={canvasRef} className='fixed w-full h-full'></canvas> {/* Setup canvas for confetti */}
+            
             <div className='w-full flex-col flex items-center pt-4 overflow-hidden'>
                 <div className='w-full flex flex-col items-center' style={{ transform: `rotate(${degree}deg)`, transition: wheelSpinning ? 'transform 3s ease-out' : 'none' }}>
                     <CircleSegments segments={segments} />
@@ -66,17 +95,22 @@ function FortuneWheel() {
             {(winningSegment && !wheelSpinning) && <p className='font-semibold text-lg mb-4'>{winningSegment}</p>}
 
             {!wheelSpinning ?
-                <Buttons>
-                    {
-                        <Button onClick={() => {
-                            spinWheel()
-                        }}>
-                            Dreh am Rad
-                        </Button>
-                    }
-                </Buttons>
+                <>
+                    <Buttons>
+                        {
+                            <Button onClick={() => {
+                                spinWheel()
+                            }}>
+                                Dreh am Rad
+                            </Button>
+                        }
+                    </Buttons>
+                </>
                 : <p className='h-4 justify-center fixed px-4 py-0 w-full left-0 space-x-0 bottom-8 md:bottom-0 md:relative'>drehen...</p>
             }
+
+            <audio ref={audioRef} src={confettiSound} />
+            <audio ref={spinningRef} src={spinningSound} />
         </div>
     )
 }
