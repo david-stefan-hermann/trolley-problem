afd9e917edcbf7ac244e3950d0815d89dde0711c
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-23 21:33:58 +0200

    removed background

--- trolley-problem-frontend/src/components/input_elements/Button.jsx
+++ trolley-problem-frontend/src/components/input_elements/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ onClick, children }) => (
     <button
-        className="font-bold bg-white text-black border-2 border-black py-2 px-2 rounded-lg transform betterhover:hover:bg-black betterhover:hover:text-white transition-colors duration-300 ease-in-out active:scale-95 active:bg-gray-300"
+        className="font-bold bg-white text-black border-2 border-black py-2 px-2 rounded-lg bg-white transform betterhover:hover:bg-black betterhover:hover:text-white transition-colors duration-300 ease-in-out active:scale-95 active:bg-gray-300"
         onClick={onClick}
     >
         {children}
--- trolley-problem-frontend/src/components/input_elements/Buttons.jsx
+++ trolley-problem-frontend/src/components/input_elements/Buttons.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Buttons = ({ children }) => (
-    <div className="flex gap-2 justify-center fixed bottom-0 bg-white px-4 py-4 md:py-0 w-full left-0 space-x-0 md:relative">
+    <div className="flex gap-2 justify-center fixed bottom-0 px-4 py-4 md:py-0 w-full left-0 space-x-0 md:relative">
         {children}
     </div>
 )
