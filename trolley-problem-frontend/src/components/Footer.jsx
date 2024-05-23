04ea13f6ee9ef02a62c9366fc2df3cc9e29f9613
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-05-23 19:12:45 +0200

    fixed footer z index

--- trolley-problem-frontend/src/components/Footer.jsx
+++ trolley-problem-frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa"
 const Footer = () => {
     return (
         <>
-            <footer className="font-medium max-w-3xl text-center mx-auto p-4 h-20">
+            <footer className="font-medium max-w-3xl text-center mx-auto p-4 h-20 z-10">
                 <span className="text-1xl">
                     <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" target="_blank" href='https://neal.fun/absurd-trolley-problems/'>neal.fun</a>
                     {' '} &#x2013; {' '}
