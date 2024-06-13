549d3772454a617572e78d3b6359b04d1b8a7069
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-06-13 21:21:05 +0200

    integrated qr display

--- trolley-problem-frontend/src/components/dashboard/DashBoard.jsx
+++ trolley-problem-frontend/src/components/dashboard/DashBoard.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react'
 import DashItem from './DashItem'
 import scenarios from '../scenarios/scenarios_data'
 import config from '../../../config'
+import QrDisplay from './QrDisplay'
+import { FaQrcode } from "react-icons/fa"
 
 const DashBoard = () => {
   const [responses, setResponses] = useState([])
   const [autoUpdate, setAutoUpdate] = useState(true)
+  const [displayQr, setDisplayQr] = useState(false)
 
   const fetchData = async () => {
     try {
@@ -51,31 +54,48 @@ const DashBoard = () => {
 
   return (
     <>
-      <div className="flex w-full h-2 border-b border-black"></div>
-      <div className="p-4 max-w-6xl">
-        <div className="flex items-center justify-between mb-4">
-          <h1 className="text-2xl font-bold md:text-3xl">Auswertung</h1>
-          <div className="flex items-center">
-            <button
-              onClick={handleDeleteVotes}
-              className="bg-red-500 text-white py-1 md:py-2 px-4 rounded"
-            >
-              Reset
-            </button>
+      {displayQr ? (
+        <p>
+          <QrDisplay hideQrCode={() => setDisplayQr(false)} />
+        </p>
+
+      ) : (
+        <>
+          <div className="flex w-full h-2 border-b border-black"></div>
+          <div className="p-4 max-w-6xl">
+            <div className="flex items-center justify-between mb-4">
+              <h1 className="text-2xl font-bold md:text-3xl">Auswertung</h1>
+              <div className="flex items-center">
+                <button
+                  onClick={() => setDisplayQr(true)}
+                  className="bg-gray-500 hover:bg-gray-600 text-white h-8 px-4 mr-3 rounded"
+                >
+                  <span>
+                    <FaQrcode />
+                  </span>
+                </button>
+                <button
+                  onClick={handleDeleteVotes}
+                  className="bg-red-500 hover:bg-red-600 text-white h-8 px-4 align-items-center rounded"
+                >
+                  Reset
+                </button>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {scenarios.map((scenario, idx) => {
+                let response = null
+                try {
+                  response = responses.find((res) => res.scenarioID === idx)
+                } catch (error) {
+                  console.error('This Index does not exist in Database.')
+                }
+                return <DashItem key={idx} scenario={scenario} response={response} />
+              })}
+            </div>
           </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {scenarios.map((scenario, idx) => {
-            let response = null
-            try {
-              response = responses.find((res) => res.scenarioID === idx)
-            } catch (error) {
-              console.error('This Index does not exist in Database.')
-            }
-            return <DashItem key={idx} scenario={scenario} response={response} />
-          })}
-        </div>
-      </div>
+        </>
+      )}
     </>
   )
 }
