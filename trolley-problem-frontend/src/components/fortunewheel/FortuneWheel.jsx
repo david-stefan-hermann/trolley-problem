8ec5cf3a97e3109918986c141f74e22065a2341c
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-06-17 00:05:52 +0200

    removed unused

--- trolley-problem-frontend/src/components/fortunewheel/FortuneWheel.jsx
+++ trolley-problem-frontend/src/components/fortunewheel/FortuneWheel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import CircleSegments from './CircleSegments'
 import Buttons from '../input_elements/Buttons'
 import Button from '../input_elements/Button'
