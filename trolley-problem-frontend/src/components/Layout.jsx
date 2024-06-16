8e3261ef97fc3687bc994af6a316ba7aef2b35d7
david.stefan.hermann <david.stefan.hermann@gmail.com>
2024-06-17 00:04:38 +0200

    added back to dash link on dashboard

--- trolley-problem-frontend/src/components/Layout.jsx
+++ trolley-problem-frontend/src/components/Layout.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Footer from './Footer'
+import { useLocation, Link } from 'react-router-dom'
 
 
 const Layout = ({ children }) => {
+    const location = useLocation()
+    const showFooter = ['/dashboard'].includes(location.pathname)
+    const showBackToDashboard = ['/spin'].includes(location.pathname)
+
     return (
         <div className="flex flex-col min-h-screen p-4 font-playpen">
+            {
+                showBackToDashboard && <Link to='/dashboard' className='text-blue-500 text-center pb-2 hover:text-blue-700'>&lt; Zurück zum Dashboard</Link>
+            }
+
             <header className="w-full max-w-3xl text-center mx-auto mb-1 md:mb-3">
                 <h1 className="text-2xl md:text-4xl font-bold">Das Trolley Problem für autonomes Fahren</h1>
             </header>
@@ -14,7 +23,9 @@ const Layout = ({ children }) => {
 
             <div className="flex w-full mb-4"></div>
 
-            <Footer />
+            <div className={`md:block ${showFooter ? '' : 'hidden'}`}>
+                <Footer />
+            </div>
         </div>
     )
 }
